ee0de3327feac902adcbc134e85608028944b578
engrshishir <shishir.cse.pstu@gmail.com>
2025-03-30 22:37:56 +0600

    [Permissions] Add ACCOUNT_ACTIVATION_PERMISSION in UserPermission

--- app/Permissions/UserPermission.php
+++ app/Permissions/UserPermission.php
@@ -5,8 +5,17 @@ namespace App\Permissions;
 
 class UserPermission extends CrudPermission
 {
+    public const ACCOUNT_ACTIVATION_PERMISSION = 'users.account_activation';
+
     public function __construct()
     {
         parent::__construct('users');
+    }
+    
+    protected function getCustomErrorMessages(): array
+    {
+        return [
+            self::ACCOUNT_ACTIVATION_PERMISSION => __("You are not allowed to do this action."),
+        ];
     }
 }
