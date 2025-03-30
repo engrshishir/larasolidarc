b310c5e59d2c64ff58b9803f82f56969881c57ac
engrshishir <shishir.cse.pstu@gmail.com>
2025-03-30 11:33:44 +0600

    [Migration]: update users table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,11 +1,12 @@
 <?php
+declare(strict_types=1);
 
+use App\Enum\DeleteStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,9 +15,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('phone', 15)->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedTinyInteger('deleted')->default(DeleteStatus::NOT_DELETED);
             $table->rememberToken();
             $table->timestamps();
         });
